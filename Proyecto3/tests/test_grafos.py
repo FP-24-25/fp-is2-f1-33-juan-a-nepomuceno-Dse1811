646c31c84085f4937500ff6910d5a54f2ceffada
Dse1811 <dasies1811@gmail.com>
2024-12-15 18:14:05 +0100

    Add files via upload

---
+++ Proyecto3/tests/test_grafos.py
@@ -0,0 +1,18 @@
+'''
+Created on 14 dic 2024
+
+@author: silva
+'''
+from entrega3.Red_social import Red_social
+
+if __name__ == '__main__':
+    rrss: Red_social = Red_social.parse('resources/usuarios.txt', 'resources/relaciones.txt')
+    if rrss:
+        sep = '\n'
+        print("************** Nº Predecesores de cada vértice")
+        print(sep.join(f'{v} -- {len(rrss.predecessors(v))}'  for v in rrss.vertex_set()))
+
+        print("\n************** Nº Vecinos de cada vértice")
+        print(sep.join(f'{v} -- {len(rrss.neighbors(v))}'  for v in rrss.vertex_set()))
+    else:
+        print("Error al crear la red social.")
